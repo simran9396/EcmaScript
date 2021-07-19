e7fd744a20b09a854c1e75a79b811e3d09012450
simran9396 <simranpathan141@gmail.com>
2021-07-19 12:39:52 +0530

    end

--- personalinfo.js
+++ personalinfo.js
@@ -1,7 +1,27 @@
-let personalinfo =[{course:"PDAC",Institute:"pixkupbiz",mobNo:9874541564, city:"Nanded"},
-                { course:"Module1",Institute:"pixkupbiz",mobNo:9874541564, city:"pune"},
-                { course:"Module2",Institute:"pixkupbiz",mobNo:9874541564, city:"Hyd"},
-                { course:"Module3",Institute:"pixkupbiz",mobNo:9874541564, city:"Mumbai"}]
-                
+const personalinfo = (data) => data.map((item)=>{
+    return`I am planning to do${item.course}from${item.Institute} situated in${item.city}contact number is${item.mobNo}`
+})
 
-                
+
+
+console.log(personalinfo([
+   { course: "PDAC",
+    Institute: "pixkupbiz",
+    mobNo: 9874541564,
+    city: "Nanded"
+}, {
+    course: "Module1",
+    Institute: "pixkupbiz",
+    mobNo: 9874541564,
+    city: "pune"
+}, {
+    course: "Module2",
+    Institute: "pixkupbiz", 
+    mobNo: 9874541564,
+    city: "Hyd"
+}, {
+    course: "Module3",
+    Institute: "pixkupbiz",
+    mobNo: 9874541564,
+    city: "Mumbai"
+}]))
